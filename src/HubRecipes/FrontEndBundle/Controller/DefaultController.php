66ab326c97d1005ccdc5b2476009b087540c446c
miezis <miezinas.mantas@gmail.com>
2014-03-25 19:30:24 +0200

    Added front-end bundle

---
+++ src/HubRecipes/FrontEndBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HubRecipes\FrontEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('HubRecipesFrontEndBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/HubRecipes/FrontEndBundle/HubRecipesFrontEndBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HubRecipes\FrontEndBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HubRecipesFrontEndBundle extends Bundle
+{
+}
