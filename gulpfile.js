cf233c1bfb99f10cedf66792dd83c2a91efd9967
elvinas <elvinoza@gmail.com>
2014-04-01 18:43:46 +0300

    initial design version

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,20 @@ gulp.task('default', function() {
         .pipe(sass())
         .pipe(gulp.dest('web/css'));
 
-    gulp.src(['app/Resources/lib/jquery/jquery.js'])
+    gulp.src(['app/Resources/lib/jquery/jquery.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/alert.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/button.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/carousel.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/dropdown.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tab.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/transition.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/scrollspy.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tooltip.js',
+            'app/Resources/lib/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/popover.js'
+        ])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('web/js'));
 
