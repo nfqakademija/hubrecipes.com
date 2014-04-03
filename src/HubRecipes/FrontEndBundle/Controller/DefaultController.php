22f2f1313ffd964fbd854528358416074d2c7a86
elvinas <elvinoza@gmail.com>
2014-04-03 19:04:58 +0300

    home page: ingridientų pridėjimas(veikia tik jei DB Ingredients lentoje yra duomenų). Veikimas: prideda tik tuos ingredientus kurie yra lentoje, neleidži prideti bet ko). Todėl įdėjau papildomai autocomplet'ą.

--- src/HubRecipes/FrontEndBundle/Controller/DefaultController.php
+++ src/HubRecipes/FrontEndBundle/Controller/DefaultController.php
@@ -2,12 +2,59 @@
 
 namespace HubRecipes\FrontEndBundle\Controller;
 
+use HubRecipes\FrontEndBundle\Entity\Ingredients;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('HubRecipesFrontEndBundle:Default:index.html.twig');
+    }
+
+    //ingredients auto complete
+    public function getIngredientsAction(){
+        if ($this->get('request')->isXmlHttpRequest()){
+            $what = $this->get('request')->request->get('data');
+
+            $em = $this->getDoctrine()->getManager();
+            $query = $em->createQuery(
+               'SELECT i.ingredient
+                FROM HubRecipes\\FrontEndBundle\\Entity\\Ingredients i
+                WHERE i.ingredient LIKE :a ')->setParameter('a','%' . $what . '%');
+
+            $products = $query->getArrayResult();
+            $a = [];
+            if (!$products) {
+
+                $a =[''];
+                return new JsonResponse(array("suggestions" => $a));
+            }
+            for($i = 0; $i< count($products); $i++){
+                $a[$i] = $products[$i]['ingredient'];
+            }
+            return new JsonResponse(array("suggestions" => $a));
+        }
+    }
+    //exists ingredient
+    public function existsAction(){
+        if ($this->get('request')->isXmlHttpRequest()){
+            $what = $this->get('request')->request->get('data');
+
+            $em = $this->getDoctrine()->getManager();
+            $query = $em->createQuery(
+                'SELECT i.ingredient
+                 FROM HubRecipes\\FrontEndBundle\\Entity\\Ingredients i
+                 WHERE i.ingredient LIKE :a ')->setParameter('a', $what );
+            $products = $query->getArrayResult();
+            if (!$products) {
+                return new JsonResponse(array("exists" => 'no'));
+            } else {
+                return new JsonResponse(array("exists" => 'yes'));
+            }
+        }
     }
 }
---
+++ src/HubRecipes/FrontEndBundle/Entity/Ingredients.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace HubRecipes\FrontEndBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ingredients
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="HubRecipes\FrontEndBundle\Entity\IngredientsRepository")
+ */
+class Ingredients
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $ingredient;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ingredient
+     *
+     * @param string $ingredient
+     * @return Ingredients
+     */
+    public function setIngredient($ingredient)
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+
+    /**
+     * Get ingredient
+     *
+     * @return string 
+     */
+    public function getIngredient()
+    {
+        return $this->ingredient;
+    }
+
+    public function __toString() {
+        return $this->getIngredient();
+    }
+}
