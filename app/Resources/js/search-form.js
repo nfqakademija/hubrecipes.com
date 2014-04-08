7c84eeed4c09af2692512e962db9c4915f182561
elvinas <elvinoza@gmail.com>
2014-04-08 10:53:36 +0300

    fix home page fix search: negalima prideti du kartus to pačio ingridiento. Surinkti paieškos duomenys

--- app/Resources/js/search-form.js
+++ app/Resources/js/search-form.js
@@ -2,6 +2,33 @@
  * Created by elvinas on 4/2/14.
  */
 
+var like_ingredients, unlike_ingredients;
+like_ingredients = new Array();
+unlike_ingredients = new Array();
+
+function exists(ing) {
+    var index = like_ingredients.indexOf(ing);
+    if (index > -1) return true;
+
+    index = unlike_ingredients.indexOf(ing);
+    if (index > -1) return true;
+
+    return false;
+}
+
+function deleteIngredient(ing, which){
+    if(which == 'like'){
+        var index = like_ingredients.indexOf(ing);
+        if (index > -1) {
+            like_ingredients.splice(index, 1);
+        };
+    } else {
+        var index = unlike_ingredients.indexOf(ing);
+        if (index > -1) {
+            unlike_ingredients.splice(index, 1);
+        };
+    }
+}
 
 $('#Sour').slider({
     formater: function (value) {
@@ -21,13 +48,25 @@ $('#Sweet').slider({
     }
 });
 
-$('#Meaty').slider({
+$('#Spicy').slider({
     formater: function (value) {
         return 'Current value: ' + value;
     }
 });
 
 $('#Bitter').slider({
+    formater: function (value) {
+        return 'Current value: ' + value;
+    }
+});
+
+$('#Savory').slider({
+    formater: function (value) {
+        return 'Current value: ' + value;
+    }
+});
+
+$('#Time').slider({
     formater: function (value) {
         return 'Current value: ' + value;
     }
@@ -45,25 +84,27 @@ $('#up-search').click(function () {
 
 //add like ingredient
 $('body').delegate('#add-ing', 'click', function () {
-    var add_rodykle, this_rodykle, k;
+    var add_rodykle, k, p;
     k = $(this).children();
     add_rodykle = $(this).parent().parent();
-    this_rodykle = $(this).parent().parent().parent();
-    $.post('/app_dev.php/exists', { data: $('input[type="text"]#ing').last().val()},
+    $.post('/app_dev.php/exists', { data: p = $('input[type="text"]#ing').last().val()},
         function (data) {
             if (data.exists == 'yes') {
-                $(k).attr('class', 'glyphicon glyphicon-minus-sign');
-                $(k).parent().attr('id', 'min-ing');
-                $('input[type="text"]#ing').last().prop('readonly', true);
-                $(add_rodykle).after($(
-                    '<div id="ing" class="col-md-3 ">' +
-                        '<form class="form-inline" role="form">' +
-                        '<div class="form-group input-group-sm">' +
-                        '<input id="ing" type="text" class="form-control " placeholder="Ingredient" autocomplete="off">' +
-                        '</div>' +
-                        '<button id="add-ing" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus-sign"></span></button>' +
-                        '</form>' +
-                        '</div>'));
+                if (!exists(p)) {
+                    like_ingredients.push(p);
+                    $(k).attr('class', 'glyphicon glyphicon-minus-sign');
+                    $(k).parent().attr('id', 'min-ing');
+                    $('input[type="text"]#ing').last().prop('readonly', true);
+                    $(add_rodykle).after($(
+                        '<div id="ing" class="col-md-3 ">' +
+                            '<form class="form-inline" role="form">' +
+                            '<div class="form-group input-group-sm">' +
+                            '<input id="ing" type="text" class="form-control " placeholder="Ingredient" autocomplete="off">' +
+                            '</div>' +
+                            '<button id="add-ing" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus-sign"></span></button>' +
+                            '</form>' +
+                            '</div>'));
+                }
             }
             if (data.exists == 'no') {
                 //galima parodyt kokia nors pagalba
@@ -74,32 +115,33 @@ $('body').delegate('#add-ing', 'click', function () {
 
 //delete ingredient
 $('body').delegate('#min-ing', 'click', function () {
-    var add_rodykle, this_rodykle, k;
-    add_rodykle = $(this).parent().parent().remove();
-    this_rodykle = $(this).parent().parent().parent();
+    $(this).parent().parent().remove();
+    deleteIngredient($(this).parent().children(".form-group").children('#ing').val(), 'like');
 });
 
 //add unlike ingredient
 $('body').delegate('#add-no-ing', 'click', function () {
-    var add_rodykle, this_rodykle, k;
+    var add_rodykle, k, p;
     k = $(this).children();
     add_rodykle = $(this).parent().parent();
-    this_rodykle = $(this).parent().parent().parent();
-    $.post('/app_dev.php/exists', { data: $('input[type="text"]#no-ing').last().val()},
+    $.post('/app_dev.php/exists', { data: p = $('input[type="text"]#no-ing').last().val()},
         function (data) {
             if (data.exists == 'yes') {
-                $(k).attr('class', 'glyphicon glyphicon-minus-sign');
-                $(k).parent().attr('id', 'min-no-ing');
-                $('input[type="text"]#no-ing').last().prop('readonly', true);
-                $(add_rodykle).after($(
-                    '<div id="no-ing" class="col-md-3 ">' +
-                        '<form class="form-inline" role="form">' +
-                        '<div class="form-group input-group-sm">' +
-                        '<input id="no-ing" type="text" class="form-control " placeholder="Ingredient" autocomplete="off">' +
-                        '</div>' +
-                        '<button id="add-no-ing" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus-sign"></span></button>' +
-                        '</form>' +
-                        '</div>'));
+                if (!exists(p)) {
+                    unlike_ingredients.push(p);
+                    $(k).attr('class', 'glyphicon glyphicon-minus-sign');
+                    $(k).parent().attr('id', 'min-no-ing');
+                    $('input[type="text"]#no-ing').last().prop('readonly', true);
+                    $(add_rodykle).after($(
+                        '<div id="no-ing" class="col-md-3 ">' +
+                            '<form class="form-inline" role="form">' +
+                            '<div class="form-group input-group-sm">' +
+                            '<input id="no-ing" type="text" class="form-control " placeholder="Ingredient" autocomplete="off">' +
+                            '</div>' +
+                            '<button id="add-no-ing" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-plus-sign"></span></button>' +
+                            '</form>' +
+                            '</div>'));
+                }
             }
             if (data.exists == 'no') {
                 //Galima parodyt kokia nors pagalba
@@ -110,9 +152,8 @@ $('body').delegate('#add-no-ing', 'click', function () {
 
 //delete unlike ingredient
 $('body').delegate('#min-no-ing', 'click', function () {
-    var add_rodykle, this_rodykle, k;
-    add_rodykle = $(this).parent().parent().remove();
-    this_rodykle = $(this).parent().parent().parent();
+    $(this).parent().parent().remove();
+    deleteIngredient($(this).parent().children(".form-group").children('#no-ing').val(), 'unlike');
 });
 
 //auto complete
@@ -128,3 +169,17 @@ $('body').delegate('input[type="text"]', 'keyup', function () {
         minLength: 1
     });
 });
+
+$('#find').click(function() {
+    var sour, salty, sweet, spicy, bitter, savory, time, type;
+    sour = $('#Sour').val();
+    salty = $('#Salty').val();
+    sweet = $('#Sweet').val();
+    spicy = $('#Spicy').val();
+    bitter = $('#Bitter').val();
+    savory = $('#Savory').val();
+    time = $('#Time').val();
+    type = $('#type').val();
+    //console.log(like_ingredients);
+    //console.log(unlike_ingredients);
+})
