eb836d14e19565bd0cc4170016487a3aab7cfd2b
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2014-05-23 20:47:10 +0200

    fixing bullshit

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,44 +10,19 @@ set :repo_url, 'https://github.com/nfqakademija/hubrecipes.com.git'
 # Default deploy_to directory is /var/www/my_app
  set :deploy_to, '/home/hubrecipes/public_html'
 
-# Default value for :scm is :git
-# set :scm, :git
+ set :deploy_to, '/home/hubrecipes/public_html'
+ set :linked_files, %w{app/config/parameters.yml}
+ set :linked_dirs, %w{app/logs app/cache vendor}
+ set :keep_releases, 5
 
-# Default value for :format is :pretty
-# set :format, :pretty
+ namespace :deploy do
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+   before :publishing, :restart
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{app/config/parameters.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{app/logs app/cache vendor}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  before :publishing, :restart
-
-  before :restart, :clear_cache do
-    on roles(:web) do
-        execute "cd #{release_path} && composer.phar install"
-        execute "cd #{release_path} && php app/console doctrine:schema:create --force"
-        execute "cd #{release_path} && php app/console cache:clear"
-        execute "cd #{release_path} && npm install"
-        execute "cd #{release_path} && bower install"
-        execute "cd #{release_path} && gulp init"
-        execute "cd #{release_path} && gulp"
-    end
-  end
-  after :finishing, 'deploy:cleanup'
-end
+   before :restart, :clear_cache do
+     on roles(:web) do
+         execute "cd #{release_path} && composer install"
+     end
+   end
+   after :finishing, 'deploy:cleanup'
+ end
